2633f2231a5f805595dd083e9d4b27a3a6dced8c
Mis cosas holberton <miscosasholberton@outlook.es>
2020-12-11 01:10:39 -0500

    Exercise 1

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+ultimoNro = int(repr(number)[-1])
+
+if ultimoNro > 5:
+    print("Last digit of {:d} is {:d} and is greater than 5".format(number, ultimoNro))
+elif ultimoNro == 0:
+    print("Last digit of {:d} is {:d} and is 0".format(number, ultimoNro))
+else:
+    print("Last digit of {:d} is {:d} and is less than 6 and not 0".format(number, ultimoNro))
